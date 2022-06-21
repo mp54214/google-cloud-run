8e118e1e266db369ae762dd09d7aca721abdd7b3
mp54214 <32453832+mp54214@users.noreply.github.com>
2022-06-21 20:41:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+    console.log("new connection")
+    res.end('hello Awesame')
+})
 
+const PORT  = process.env.PORT || 8080
+server.listen(PORT, () => console.log('Listening'))
